b5056b3d9f03d2cfc05e7af3f9bcaee1e012f174
hevslc <hevelyn.sthefany@gmail.com>
2020-12-02 15:21:34 -0300

    funçoes do pandas rodam. Problema detectado

--- r2a/r2apandas.py
+++ r2a/r2apandas.py
@@ -36,7 +36,7 @@ class r2aPandas(IR2A):
         self.send_up(msg)
 
     def handle_segment_size_request(self, msg):
-        msg.add_quality_id(self.pandas.r[-1])
+        msg.add_quality_id(self.pandas.get_quality())
         self.send_down(msg)
 
     def handle_segment_size_response(self, msg):
@@ -47,13 +47,14 @@ class r2aPandas(IR2A):
         pass
 
     def finalization(self):
-        pass
+        for xn in self.pandas.x:
+            print(xn)
 
 
 class Pandas:
-    def __init__(self, w=0.3, k=0.14, beta=0.2, alfa=0.2, e=0.15, t=0, b=0, 
-                 bmin=26, r = [0,0], deltaup=0, deltadown=0, trequest=0, tresponse=0, 
-                 n=-1, tnd=[0], tr=[], td=[0], x=[]):
+    def __init__(self, w=0.3, k=0.14, beta=0.2, alfa=0.2, e=0.15, t=0, b=[], 
+                 bmin=26, r = [], deltaup=0, deltadown=0, trequest=0, tresponse=0, 
+                 n=-1, tnd=[0], tr=[], td=[0], x=[], y=[], z=[], qi=np.array([])):
         self.trequest = trequest # momento em que foi realizado o request
         self.tresponse = tresponse # momento em que foi recebida a resposta
         self.n     = n     # numero do segmento
@@ -70,9 +71,9 @@ class Pandas:
         self.tr    = tr    # tempo real entre as solicitações
         self.td    = td    # duração do download
         self.x     = x     # taxa média de dados alvo (ou compartilhamento de largura de banda)
-        self.y     = []    # versão suavizada de x
-        self.z     = []    # taxa de tranferência de TCP medida z=rt/T'
-        self.qi    = np.array([])    # Conjunto de taxas de bits de vídeo
+        self.y     = y     # versão suavizada de x
+        self.z     = z     # taxa de tranferência de TCP medida z=rt/T'
+        self.qi    = qi    # Conjunto de taxas de bits de vídeo
         self.deltaup   = deltaup     # margem de segurança para cima 
         self.deltadown = deltadown   # margem de segurança para baixo
 
@@ -84,23 +85,24 @@ class Pandas:
         x0 = self.qi[int(idx_x0)]
         self.x.append(x0)
         self.y.append(x0)
-        self.r[0] = int(idx_x0) #inicializando qualidade inicial
-        self.r[1] = x0    
+        self.r.append(x0)
+        self.b.append((1-self.r[-1]/self.y[-1])*self.t/self.beta + self.bmin)
+        self.tTarget_inter_request()
 
     # Estimativa da porção da largura de banda
-    '''
+    
     def estimate_xn(self):
-        self.tr.append(max(self.tnd[-1], self.td[-1])) #na primeira vez daria quanto?????
+        self.tr.append(max(self.tnd[-1], self.td[-1])) #na primeira vez daria Bmin
         m = max(0, self.x[-1]-self.z[-1]+self.w) 
-        xn = self.x[-1] + self.k*self.tnd[-1]*(self.w - m) #TND n deveria ser TR?????
+        xn = self.x[-1] + self.k*self.tr[-1]*(self.w - m) 
         self.x.append(xn)
     
     def get_quality(self):
         self.estimate_xn()
         self.S()
         self.Q()
-        tTarget_inter_request()
-        return self.r[-1] # ERRO, r definido como inteiro 
+        self.tTarget_inter_request()
+        return self.r[-1]
 
     def S(self): #EWMA smoother
         self.y.append(self.y[-1] - self.tr[-1] * self.alfa * (self.y[-1] - self.x[-1]))
@@ -110,17 +112,43 @@ class Pandas:
         rup = self.get_rup()
         rdown = self.get_rdown()
         rn = 0
-        if(self.r[-1] < rup): rn = rup # ERRADO, r definido como um inteiro
-        elif(rup <= self.r[-1] and self.r[-1] <= rdown): rn = self.r[-1] #MESMO ERRO
+        if(self.r[-1] < rup): rn = rup 
+        elif(rup <= self.r[-1] and self.r[-1] <= rdown): rn = self.r[-1] 
         else: rn = rdown 
-        self.r.append(rn) # MESMO ERRO
-    
+        self.r.append(rn) 
+   
     def get_rup(self):
-        y = self.y[-1] - self.deltaup #Chamando de y pq?? Pra confundir????
-        qi2 = self.qi[self.qi <= y] #ERRADO / guardar indice em vez de valor
+        y2 = self.y[-1] - self.deltaup
+        qi2 = self.qi[self.qi <= y2] 
+        if(qi2.size): return qi2[-1]
+        else:         return self.qi[0]
 
-        return qi2[-1]
+    def get_rdown(self):
+        y2 = self.y[-1] - self.deltadown 
+        '''print("y2: ", y2)
+        for q in self.qi:
+            print(q)'''
+        qi2 = self.qi[self.qi <= y2] 
+        if(qi2.size): return qi2[-1]
+        else:         return self.qi[0]
 
+
+    def tTarget_inter_request(self):
+        tnd = self.r[-1]*self.t/self.y[-1] + self.beta*(self.b[-1] - self.bmin)
+        self.tnd.append(tnd)
+
+    def update_response(self, actual_tresponse):
+        self.tresponse = actual_tresponse
+        self.td.append(self.tresponse - self.trequest) # tempo do download do segmento
+        self.z.append((self.r[-1]*self.t)/self.td[-1]) # valor do throughput TCP real
+    
+    def update_request(self, actual_trequest, buffer_size):
+        self.n += 1
+        self.trequest = actual_trequest
+        self.b.append(buffer_size )
+
+
+'''
         #Sugestao: 
         i2 = 0
         qi2 = 0
@@ -130,35 +158,4 @@ class Pandas:
             if qi2 == q:
                 i2 = i
         return i2
-    
-    def get_rdown(self):
-        y = self.y[-1] - self.deltadown #Chamando de y pq?? Pra confundir????
-        qi2 = self.qi[self.qi <= y] # MESMA COISA DA UP
-        return qi2[-1]
-
-    def tTarget_inter_request(self):
-        if(self.n==0): self.b = (1-self.r[-1]/self.y[-1])*self.t/self.beta + self.bmin #USANDO r COMO VETOR / quando isso seria vdd???
-        tnd = self.r[-1]*self.t/self.y[-1] + self.beta*(self.b[-1] - self.bmin) #USANDO r e b COMO VETOR
-        self.tnd.append(tnd)
-
-    def put_tresponse(self):
-        pass
-
-    def get_rate(self):
-        pass
-
-    def get_buffersize(self, b):
-        self.b = b
-        pass
-    '''
-    def update_response(self, actual_tresponse):
-        self.tresponse = actual_tresponse
-        self.td.append(self.tresponse - self.trequest) # tempo do download do segmento
-        self.z.append((self.r[1]*self.t)/self.td[-1]) # valor do throughput TCP real
-    
-    def update_request(self, actual_trequest, buffer_size):
-        self.n += 1
-        self.trequest = actual_trequest
-        self.b = buffer_size 
-
-
+'''  
